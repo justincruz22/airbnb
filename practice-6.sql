c4a94153b3a1687bfa77c83730e48db5373367e0
justincruz22 <justin.cruz1@kellogg.northwestern.edu>
2022-01-18 00:17:13 +0000

    week 2 practice!

--- practice-6.sql
+++ practice-6.sql
@@ -16,3 +16,7 @@
 -- | Burnside               | 10       |
 
 
+SELECT listings.neighborhood, count(*) 
+FROM reviews 
+INNER JOIN listings on listings.id = reviews.listing_id
+GROUP BY listings.neighborhood;
